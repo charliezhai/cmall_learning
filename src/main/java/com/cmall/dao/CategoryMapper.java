368b75a79cf917da59bc0bb5183cf26247c07eae
Charlie Zhai <charliezhai@CharliedeMacBook-Air.local>
2017-07-26 15:06:43 +0800

    project init commit

---
+++ src/main/java/com/cmall/dao/CategoryMapper.java
@@ -0,0 +1,17 @@
+package com.cmall.dao;
+
+import com.cmall.pojo.Category;
+
+public interface CategoryMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    Category selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
+}
