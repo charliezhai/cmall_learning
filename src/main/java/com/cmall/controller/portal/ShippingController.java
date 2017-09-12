fd67fa6372073a4800634ff0aae815893ed6916a
Charlie Zhai <charliezhai@CharliedeMacBook-Air.local>
2017-09-12 14:26:14 +0800

    project init commit

---
+++ src/main/java/com/cmall/controller/portal/ShippingController.java
@@ -0,0 +1,110 @@
+package com.cmall.controller.portal;
+
+import com.cmall.common.Const;
+import com.cmall.common.ResponseCode;
+import com.cmall.common.ServerResponse;
+import com.cmall.pojo.Shipping;
+import com.cmall.pojo.User;
+import com.cmall.service.IShippingService;
+import com.github.pagehelper.PageInfo;
+import com.mysql.fabric.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/shipping/")
+public class ShippingController {
+
+    @Autowired
+    private IShippingService iShippingService;
+
+    /**
+     * 增加地址
+     * @param session
+     * @param shipping
+     * @return
+     */
+    @RequestMapping("add.do")
+    @ResponseBody
+    public ServerResponse add(HttpSession session, Shipping shipping){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.add(user.getId(),shipping);
+    }
+
+    /**
+     * 删除地址
+     * @param session
+     * @param shippingId
+     * @return
+     */
+    @RequestMapping("delete.do")
+    @ResponseBody
+    public ServerResponse delete(HttpSession session, Integer shippingId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.delete(user.getId(),shippingId);
+    }
+
+    /**
+     * 更新地址
+     * @param session
+     * @param shipping
+     * @return
+     */
+    @RequestMapping("update.do")
+    @ResponseBody
+    public ServerResponse update(HttpSession session, Shipping shipping){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.update(user.getId(),shipping);
+    }
+
+    /**
+     * 查询地址
+     * @param session
+     * @param shippingId
+     * @return
+     */
+    @RequestMapping("select.do")
+    @ResponseBody
+    public ServerResponse<Shipping> select(HttpSession session, Integer shippingId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.select(user.getId(),shippingId);
+    }
+
+    /**
+     * 分页
+     * @param pageNum
+     * @param pageSize
+     * @param session
+     * @return
+     */
+    @RequestMapping("list.do")
+    @ResponseBody
+    public ServerResponse<PageInfo> list(@RequestParam(value="pageNum",defaultValue="1")int pageNum,
+                                         @RequestParam(value="pageSize",defaultValue="10")int pageSize,
+                                         HttpSession session){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.list(user.getId(),pageNum,pageSize);
+    }
+
+
+}
---
+++ src/main/java/com/cmall/service/IShippingService.java
@@ -0,0 +1,18 @@
+package com.cmall.service;
+
+import com.cmall.common.ServerResponse;
+import com.cmall.pojo.Shipping;
+import com.github.pagehelper.PageInfo;
+
+public interface IShippingService {
+
+    ServerResponse add(Integer userId, Shipping shipping);
+
+    ServerResponse delete(Integer userId,Integer shippingId);
+
+    ServerResponse update(Integer userId,Shipping shipping);
+
+    ServerResponse<Shipping> select(Integer userId,Integer shippingId);
+
+    ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize);
+}
